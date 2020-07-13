a2ff89b9b32e42db806e18d42af21cffa503cdb6
exquizzle <40224753+exquizzle@users.noreply.github.com>
2020-07-13 14:25:41 -0400

    initial commit

---
+++ tree.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+// const d3 = require('d3');
+// import * as d3 from './d3/d3';
+// import * as flare from './flare';
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+
+  // const test = document.getElementById('testDiv');
+  // const title = document.createElement('h3');
+  // title.innerHTML = 'adding a tltle test';
+  // test.appendChild(title);
+
+  const svg = d3.select('body')
+    .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
+
+});
+
+console.log('hello world');
+console.log(flare);
+// export default tree;
